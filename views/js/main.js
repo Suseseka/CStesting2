9200baafc79115579af39aa19e46be71f77a0474
AzamatErzhanov <azamat.erzhanov@madnesspartners.com>
2025-10-21 10:30:01 +0000

    1

--- views/js/main.js
+++ views/js/main.js
@@ -469,7 +469,6 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    elem.style.position = 'absolute';
     document.querySelector("#movingPizzas1").appendChild(elem);
   }
   updatePositions();
